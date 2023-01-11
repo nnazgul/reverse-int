d5f01abd7ff090977738da0299254717c9afdf03
nnazgul <nazgul.nurmatova.ch@gmail.com>
2023-01-12 01:36:56 +0600

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let input = String(n);
+    let output = [];
+    for (let i = input.length; i >= 0; i--) {
+        console.log(output.push(input[i]));
+    }
+    let result = output.toLocaleString().replace(/\,/g, '');
+    return Number(result.replace(/\-/g, ''));
 }
